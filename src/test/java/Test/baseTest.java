2a302b3e4b3451d15f5437ac3af778f1ca164d74
DaffaPintu <daffa@pintu.co.id>
2024-05-14 12:26:15 +0700

    FIX

--- src/test/java/Test/baseTest.java
+++ src/test/java/Test/baseTest.java
@@ -1,22 +1,22 @@
 package Test;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 public class baseTest {
 
     protected static WebDriver driver;
     protected void getDriver(){
-        ChromeOptions option = new ChromeOptions();
-        option.addArguments("--no-sandbox");
-        option.addArguments("--disable-dev-shm-usage");
-        option.addArguments("--remote-allow-origins=*");
-        option.addArguments("--window-size=1920,1080");
-        option.addArguments("--headless");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(option);
-
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
     }
 }
